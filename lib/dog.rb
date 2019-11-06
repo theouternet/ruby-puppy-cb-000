1232d8c8d13c9910a000612f050a91e2f8d29ed2
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2019-11-06 01:51:08 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,30 @@
-# Add your code here
+class Dog 
+  
+  @@all = []
+  
+  attr_accessor :name
+  
+  def initialize(name)
+    @name = name
+    save
+  end
+  
+  def self.all 
+    return @@all 
+  end
+  
+  def self.clear_all
+    @@all = []
+  end
+  
+  def self.print_all
+    @@all.each do |n|
+      puts n.name
+    end
+  end
+  
+  def save
+    @@all << self
+  end
+  
+end
